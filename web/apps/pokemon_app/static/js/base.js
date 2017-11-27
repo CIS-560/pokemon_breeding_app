8df0f6a0c3b19a44afe536b88de3e4f506025ab6
Paula Mendez <paulipotter@gmail.com>
2017-11-27 11:51:20 -0600

    failing at js and css

--- web/apps/pokemon_app/static/js/base.js
+++ web/apps/pokemon_app/static/js/base.js
@@ -1,6 +1,7 @@
 //src : https://foundation.zurb.com/forum/posts/48054-autocomplete-search-field
-$( '#iconTag' ).autocomplete({
-    source: availableTags = ["rendered list of pokemons", "pokemons", "porygon", "porygon2", "pikachu"];
-    minLength: 3,
-    appendTo: "frmCriteria"
+
+$(document).ready(function() {
+    //Disable the selection button on egg moves by default
+    $('#search-parents-button').prop('disabled', true);
+    $('#egg-move-select').prop('disabled', true);
 });
